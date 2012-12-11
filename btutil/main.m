2a544127b87ff8f6edf6d7d9160b054993d3679f
Marshall Brekka <marshallsmedia@gmail.com>
2012-12-10 20:15:32 -0800

    Initial Commit

---
+++ btutil/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  btutil
+//
+//  Created by Marshall Brekka on 12/10/12.
+//  Copyright (c) 2012 Marshall Brekka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
