b1bc7c3b65f5eff512d7d14cf8a7d52f28b7bbaa
Marshall Brekka <marshallsmedia@gmail.com>
2012-12-12 20:56:57 -0800

    first commit, a bluetooth command line tool to view/connect/disconnect paired devices

--- btutil/main.m
+++ btutil/main.m
@@ -7,15 +7,47 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <IOBluetooth/IOBluetooth.h>
+#import "BTDevice.h"
+
+void printUsage(const char * binName) {
+    printf("Usage: %s [list [<address>]] [connect|disconnect <address>]\n", binName);
+}
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        if(argc == 1) {
+            printUsage(argv[0]);
+            return 1;
+        }
+        NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+        NSString *task = [arguments objectAtIndex:1];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        if([task isEqualToString:@"list"]) {
+            NSArray *devices = [IOBluetoothDevice pairedDevices];
+            NSString *address;
+            if (argc == 3) {
+                address = [arguments objectAtIndex:2];
+            }
+            for(id object in devices) {
+                if (argc == 3) {
+                    if([[object addressString] isEqualToString:address]) {
+                        [BTDevice list:object];
+                    }
+                } else {
+                    [BTDevice list:object];
+                }
+                
+            }
+        } else if ([task isEqualToString:@"connect"] && argc == 3) {
+            [BTDevice connectAddress:[arguments objectAtIndex:2]];
+        } else if ([task isEqualToString:@"disconnect"] && argc == 3) {
+            [BTDevice disconnectAddress:[arguments objectAtIndex:2]];
+        } else {
+            printUsage(argv[0]);
+        }
     }
     return 0;
 }
